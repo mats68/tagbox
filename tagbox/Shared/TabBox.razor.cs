47e5d08ca26abdc10dd90f82a6fe4d4f7a2d39ce
M. Thaler <vs.schema.form@gmail.com>
2020-05-12 08:35:58 +0200

    Add project files.

---
+++ tagbox/Shared/TabBox.razor.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tagbox.Shared
+{
+    public class TabBoxBase : ComponentBase
+    {
+        public string Skills { get; set; } = "HTML,CSS";
+
+        public void TagsChanged(IEnumerable<string> newTags)
+        {
+            if (newTags != null)
+            {
+                Skills = string.Join(',', newTags);
+                SkillData = Skills.Split(',').ToList();
+                SkillSelected = Skills.Split(',').ToList();
+                InvokeAsync(StateHasChanged);
+            }
+            else
+            {
+                Skills = null;
+            }
+
+        }
+        public IEnumerable<string> SkillData = new List<string>();
+        public IEnumerable<string> SkillSelected = new List<string>();
+
+        protected override void OnInitialized()
+        {
+            if (Skills != null)
+            {
+                SkillData = Skills.Split(',').ToList();
+                SkillSelected = Skills.Split(',').ToList();
+            }
+
+        }
+
+
+    }
+
+
+}
